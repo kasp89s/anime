6a4f9f96262214782866931b7126ded468ad93c5
Sergey Kaspruk <s.kaspryk@emict.com>
2016-11-21 18:40:23 +0200

    21.11

---
+++ modules/admin/models/search/OrderSearch.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\modules\admin\models\search;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Order;
+
+/**
+ * OrderSearch represents the model behind the search form about `app\models\Order`.
+ */
+class OrderSearch extends Order
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'customerId', 'shippingId', 'paymentId', 'isFinished'], 'integer'],
+            [['currencyCode', 'orderStatus', 'couponCode', 'createTime', 'updateTime'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Order::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'customerId' => $this->customerId,
+            'shippingId' => $this->shippingId,
+            'paymentId' => $this->paymentId,
+            'createTime' => $this->createTime,
+            'updateTime' => $this->updateTime,
+            'isFinished' => $this->isFinished,
+        ]);
+
+        $query->andFilterWhere(['like', 'currencyCode', $this->currencyCode])
+            ->andFilterWhere(['like', 'orderStatus', $this->orderStatus])
+            ->andFilterWhere(['like', 'couponCode', $this->couponCode]);
+
+        return $dataProvider;
+    }
+}
---
+++ modules/admin/models/search/OrderStatusSearch.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\modules\admin\models\search;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\OrderStatus;
+
+/**
+ * OrderStatusSearch represents the model behind the search form about `app\models\OrderStatus`.
+ */
+class OrderStatusSearch extends OrderStatus
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'isDefault', 'isChargeble', 'isPaid', 'isShipped', 'isRestock', 'isPenalty', 'isFinished'], 'integer'],
+            [['statusCode', 'name'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = OrderStatus::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'isDefault' => $this->isDefault,
+            'isChargeble' => $this->isChargeble,
+            'isPaid' => $this->isPaid,
+            'isShipped' => $this->isShipped,
+            'isRestock' => $this->isRestock,
+            'isPenalty' => $this->isPenalty,
+            'isFinished' => $this->isFinished,
+        ]);
+
+        $query->andFilterWhere(['like', 'statusCode', $this->statusCode])
+            ->andFilterWhere(['like', 'name', $this->name]);
+
+        return $dataProvider;
+    }
+}
