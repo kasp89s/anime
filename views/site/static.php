563f1255a5abb8357a4510335346838c029c81a7
Turbo <kasp89s@gmail.com>
2016-11-27 15:41:07 +0200

    path 27.11

--- views/site/static.php
+++ views/site/static.php
@@ -6,6 +6,6 @@ use yii\widgets\Breadcrumbs;
         <?php echo Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [], 'homeLink' => false]);?>
     </ul>
 </div>
-<div class="content-container clearfix">
+<div class="content-container static clearfix">
     <?php echo $page->content ?>
 </div>
